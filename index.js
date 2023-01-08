25bb7553eef001ee801a9dbb059cfcde2fbc0430
Matthew <turbodieseldodge@gmail.com>
2023-01-08 17:46:49 -0600

    extra functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(chore = "go to the office") {
+    return `This Monday, I will ${chore}.`
+}
+
+function wrapAdjective(special = "*") {
+    return function (praise = "special") {
+        return (`You are ${special}${praise}${special}!`)
+    }
+}
